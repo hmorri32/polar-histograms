052591a1e2707739db5e956fb4bea11769758bfd
Hugh Morrison <hmorri32@gmail.com>
2018-08-14 15:51:35 -0600

    init mapbox gl and add pitch and bearing

---
+++ src/index.js
@@ -0,0 +1,66 @@
+
+import { CREDENTIALS } from '../credentials';
+import mapboxgl from 'mapbox-gl';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+mapboxgl.accessToken = CREDENTIALS.mapboxgl;
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  // style: 'mapbox://styles/mapbox/satellite-streets-v9',
+  style: 'mapbox://styles/mapbox/outdoors-v9',
+  center: [-104.9903, 39.7392],
+  zoom: 9,
+  hash: true,
+});
+
+map.addControl(
+  new MapboxGeocoder({ accessToken: mapboxgl.accessToken }),
+  'top-right'
+);
+
+map.addControl(new mapboxgl.NavigationControl(), 'top-right');
+
+map.on('load', function() {
+  const la = map.getStyle().layers;
+  const layers = la.filter(l => l.type === 'symbol' && l.layout['text-field']);
+  const layerId = layers[0].id;
+  map.addLayer(
+    {
+      id: '3d-buildings',
+      source: 'composite',
+      'source-layer': 'building',
+      filter: ['==', 'extrude', 'true'],
+      type: 'fill-extrusion',
+      minzoom: 12,
+      paint: {
+        'fill-extrusion-color': '#FFFFFF',
+        'fill-extrusion-height': [
+          'interpolate',
+          ['linear'],
+          ['zoom'],
+          15,
+          0,
+          15.05,
+          ['get', 'height'],
+        ],
+        'fill-extrusion-base': [
+          'interpolate',
+          ['linear'],
+          ['zoom'],
+          15,
+          0,
+          15.05,
+          ['get', 'min_height'],
+        ],
+        'fill-extrusion-opacity': 0.6,
+      },
+    },
+    layerId
+  );
+});
+
+map.on('zoom', () => {
+  map.getZoom() > 14
+    ? map.easeTo({ pitch: 40, bearing: -17 })
+    : map.easeTo({ pitch: 0, bearing: 0 });
+});
