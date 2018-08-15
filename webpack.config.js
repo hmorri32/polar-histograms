170d5662e06f61e65331391029a2602873cec0a6
Hugh Morrison <hmorri32@gmail.com>
2018-08-15 17:33:19 -0600

    adds babel preset webpackconfig

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,17 @@ module.exports = {
   mode: 'development',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
 };
